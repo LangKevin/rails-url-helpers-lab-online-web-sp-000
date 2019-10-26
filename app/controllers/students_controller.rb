aec23c4577b9cc931db780b805027c537e3b9f92
kevin lang <langkevina@gmail.com>
2019-10-26 20:12:33 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,11 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find(params[:id])
-    @student.active = !@student.active
+    if !@student.active
+      @student.active = false
+    elsif 
+      @student.active = true
+    end if
     @student.save
     redirect_to student_path(@student)
   end
